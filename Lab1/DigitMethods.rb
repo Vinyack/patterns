70c25afa1a4faa002afa7fdb53e9e01113d29e1a
Vinyack <vinyackofficial@gmail.com>
2024-09-14 02:53:04 +0300

    Поменял название переменной n на number

--- Lab1/DigitMethods.rb
+++ Lab1/DigitMethods.rb
@@ -2,9 +2,9 @@
 # МЕТОД 1
 def sum_of_non_prime_divisors
   print "Введите число: "
-  n = gets.to_i
+  number = gets.to_i
   # Проверка, является ли число простым
-  sum = (1..n).select { |i| n % i == 0 && (i <= 1 || (2..Math.sqrt(i)).any? { |d| i % d == 0 }) }.sum
-  puts "Сумма непростых делителей числа #{n} равна #{sum}"
+  sum = (1..number).select { |i| number % i == 0 && (i <= 1 || (2..Math.sqrt(i)).any? { |d| i % d == 0 }) }.sum
+  puts "Сумма непростых делителей числа #{number} равна #{sum}"
 end
 sum_of_non_prime_divisors
