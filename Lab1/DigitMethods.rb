e04f91d2dc081b5d2b0f8d4427273f2d03f778db
Vinyack <vinyackofficial@gmail.com>
2024-09-20 23:11:49 +0300

    add second method in task#4

--- Lab1/DigitMethods.rb
+++ Lab1/DigitMethods.rb
@@ -1,3 +1,4 @@
+# ЗАДАНИЕ 4
 # ВАРИАНТ 6
 # МЕТОД 1
 def sum_of_non_prime_divisors
@@ -8,3 +9,13 @@ def sum_of_non_prime_divisors
   puts "Сумма непростых делителей числа #{number} равна #{sum}"
 end
 sum_of_non_prime_divisors
+# МЕТОД 2
+def count_digits_less_than_3(number)
+  # Преобразуем число в строку, а затем в массив цифр
+  digits = number.to_s.chars
+  # Подсчитываем цифры, которые меньше 3
+  digits.count { |digit| digit.to_i < 3 }
+end
+print "Введите число: "
+number = gets.chomp.to_i
+puts "Количество цифр меньше 3: #{count_digits_less_than_3(number)}"
