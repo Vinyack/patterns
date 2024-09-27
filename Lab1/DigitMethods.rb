7c3949a7b7915172acdb95155ffca5440eb63418
Vinyack <vinyackofficial@gmail.com>
2024-09-27 17:41:09 +0300

    add method 3 in task 4

--- Lab1/DigitMethods.rb
+++ Lab1/DigitMethods.rb
@@ -1,4 +1,5 @@
 # ЗАДАНИЕ 4
+require 'prime'  # Подключаем модуль для работы с простыми числами (для 3-го метода)
 # ВАРИАНТ 6
 # МЕТОД 1
 def sum_of_non_prime_divisors
@@ -20,15 +21,46 @@ print "Введите число: "
 number = gets.chomp.to_i
 puts "Количество цифр меньше 3: #{count_digits_less_than_3(number)}"
 # МЕТОД 3
+require 'prime'  # Подключаем модуль для работы с простыми числами
 # алгоритм Евклида
-def gcd(a,b)
-  if b == 0
-    return a
-  else 
-    return gcd(b, a%b)
+def gcd(a, b)
+  # Используем цикл для нахождения НОД
+  while b != 0
+    a, b = b, a % b  # Присваиваем a значение b, а b — остаток от деления a на b
   end
+  return a
 end
-# функция возвращает true если два числа взаимно простые, то есть НОД == 1
-def mutually_prime?(a,b)
-  gcd(a,b)==1
+# функция для вычисления суммы простых цифр числа
+def prime_digit_sum(number)
+  digits = number.digits  # Разбиваем число на массив его цифр
+  sum = 0  # Переменная для хранения суммы простых цифр
+
+  # проходим по каждой цифре числа
+  for digit in digits
+    # проверяем, является ли цифра простой
+    if Prime.prime?(digit)
+      sum += digit  # если цифра простая, добавляем её к сумме
+    end
+  end
+
+  return sum  # Возвращаем итоговую сумму простых цифр
 end
+# функция для решения задачи
+def solve(num)
+  prime_sum = prime_digit_sum(num)  # Вычисляем сумму простых цифр числа
+
+  count = 0  # Переменная для подсчёта подходящих чисел
+
+  # Проходим по всем числам от 1 до num-1 и проверяем условия
+  for i in 1...num
+    if (num % i != 0) && (gcd(num, i) != 1) && (gcd(prime_sum, i) == 1)
+      count += 1  # Если все условия выполнены, увеличиваем счётчик
+    end
+  end
+
+  return count
+end
+puts "Введите число: "
+number = gets.to_i
+result = solve(number)
+puts "Результат: #{result}"
