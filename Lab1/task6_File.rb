764edd386bc8f6f6888cf6390228f816599a1f51
Vinyack <vinyackofficial@gmail.com>
2024-10-05 00:25:33 +0300

    lab6_change_methods

--- Lab1/task6_File.rb
+++ Lab1/task6_File.rb
@@ -1,64 +1,47 @@
-require 'prime'
 # Функция для нахождения наибольшего общего делителя (НОД)
 def gcd(a, b)
-  while b != 0
-    a, b = b, a % b
-  end
-  return a
+  b == 0 ? a : gcd(b, a % b)
+end
+
+# Функция для проверки, является ли число простым
+def prime?(number)
+  return false if number < 2
+  (2..Math.sqrt(number)).none? { |i| number % i == 0 }
 end
 
 # Функция для вычисления суммы простых цифр числа
 def prime_digit_sum(number)
-  # Если число отрицательное, преобразуем его в положительное
-  number = number.abs
-  
-  digits = number.digits
-  sum = 0
-  for digit in digits
-    if Prime.prime?(digit)
-      sum += digit
-    end
-  end
-  return sum
+  number.abs.digits.select { |digit| prime?(digit) }.sum
 end
 
 # Функция для решения задачи
 def solve(num)
   prime_sum = prime_digit_sum(num)
-  count = 0
-  for i in 1...num
-    if (num % i != 0) && (gcd(num, i) != 1) && (gcd(prime_sum, i) == 1)
-      count += 1
-    end
+  (1...num).count do |i|
+    num % i != 0 && gcd(num, i) != 1 && gcd(prime_sum, i) == 1
   end
-  return count
 end
 
-# Функция для выполнения заданного метода
+# Выполнение задачи на основе переданного метода
 def perform_task(method, numbers)
-  case method
-  when 'gcd'
-    result = gcd(numbers[0], numbers[1])
-  when 'prime_digit_sum'
-    result = prime_digit_sum(numbers[0])
-  when 'solve'
-    result = solve(numbers[0])
-  else
-    puts "Неизвестный метод: #{method}"
-    return
-  end
+  result =
+    case method
+    when 'gcd'
+      gcd(numbers[0], numbers[1])
+    when 'prime_digit_sum'
+      prime_digit_sum(numbers[0])
+    when 'solve'
+      solve(numbers[0])
+    else
+      puts "Неизвестный метод: #{method}"
+      return
+    end
   puts "Результат: #{result}"
 end
 
-# Чтение данных из файла
+# Чтение чисел из файла
 def read_numbers_from_file(filename)
-  numbers = []
-  File.open(filename, "r") do |file|
-    file.each_line do |line|
-      numbers << line.to_i
-    end
-  end
-  return numbers
+  File.readlines(filename).map(&:to_i)
 end
 
 # Основная программа
