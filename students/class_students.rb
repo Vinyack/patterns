59b15b1275425336ed3e5c9fa2d98199b5922a8a
Vinyack <VinyackOfficial@gmail.com>
2024-11-02 07:00:11 +0300

    extract_common_code_to_superclass_person

--- students/class_students.rb
+++ students/class_students.rb
@@ -1,19 +1,15 @@
-class Student
-  # Определяем геттеры и сеттеры для всех полей
-  attr_accessor :id, :telegram, :email, :git
-  attr_reader :last_name, :first_name, :middle_name, :phone
+require_relative 'person'
 
-  # Конструктор с именованными аргументами
+class Student < Person
+  attr_accessor :telegram, :email, :git
+  attr_reader :phone
+
   def initialize(id: nil, last_name:, first_name:, middle_name:, phone: nil, telegram: nil, email: nil, git: nil)
-    @id = id
-    self.last_name = last_name
-    self.first_name = first_name
-    self.middle_name = middle_name
+    super(id: id, last_name: last_name, first_name: first_name, middle_name: middle_name)
     self.phone = phone
     self.telegram = telegram
     self.email = email
     self.git = git
-    @contacts_frozen = false
   end
 
   # Дополнительный конструктор, принимающий строку
@@ -45,39 +41,18 @@ class Student
     else
       {}
     end
-
-  # Валидация обязательных полей ФИО
-  def last_name=(value)
-    @last_name = validate_presence(:last_name, value)
   end
 
-  def first_name=(value)
-    @first_name = validate_presence(:first_name, value)
-  end
-
-  def middle_name=(value)
-    @middle_name = validate_presence(:middle_name, value)
-  end
-
-  # Метод для проверки наличия значения для обязательных полей
-  def validate_presence(field, value)
-    raise ArgumentError, "#{field.capitalize} не может быть пустым" if value.nil? || value.strip.empty?
-    value
-  end
-
-  # Валидация и установка необязательных полей
+  # Метод для валидации и установки контактов
   def phone=(value)
-    raise "Невозможно изменить контакты" if contacts_frozen?
-    @phone = value.nil? || Student.valid_phone?(value) ? value : raise(ArgumentError, "Неверный формат телефона")
+    @phone = value.nil? || valid_phone?(value) ? value : raise(ArgumentError, "Неверный формат телефона")
   end
 
   def telegram=(value)
-    raise "Невозможно изменить контакты" if contacts_frozen?
     @telegram = value.nil? || valid_telegram?(value) ? value : raise(ArgumentError, "Неверный формат Telegram")
   end
 
   def email=(value)
-    raise "Невозможно изменить контакты" if contacts_frozen?
     @email = value.nil? || valid_email?(value) ? value : raise(ArgumentError, "Неверный формат email")
   end
 
@@ -86,7 +61,7 @@ class Student
   end
 
   # Методы проверки форматов
-  def self.valid_phone?(phone)
+  def valid_phone?(phone)
     phone.match?(/^\+?\d{10,15}$/)
   end
 
@@ -102,61 +77,13 @@ class Student
     git.match?(/^github\.com\/[\w.-]+$/)
   end
 
-  # Метод validate для проверки наличия гита и хотя бы одного контакта
-  def validate
-    if git.nil? || (phone.nil? && telegram.nil? && email.nil?)
-      raise "Ошибка: у студента должен быть либо GitHub, либо хотя бы один контактный метод"
-    end
-  end
-
-  # Метод set_contacts для установки контактов и заморозки их значений
-  def set_contacts(phone: nil, telegram: nil, email: nil)
-    @phone = phone if phone && Student.valid_phone?(phone)
-    @telegram = telegram if telegram && valid_telegram?(telegram)
-    @email = email if email && valid_email?(email)
-    
-    @contacts_frozen = true
-    freeze_contacts
-  end
-
-  private
-
-  def freeze_contacts
-    @phone.freeze if @phone
-    @telegram.freeze if @telegram
-    @email.freeze if @email
-  end
-
-  def contacts_frozen?
-    @contacts_frozen
-  end
-
-  # Метод для отображения информации об объекте
-  def display_info
-    puts "ID: #{@id || 'не указан'}"
-    puts "Фамилия: #{@last_name}"
-    puts "Имя: #{@first_name}"
-    puts "Отчество: #{@middle_name}"
-    puts "Телефон: #{@phone || 'не указан'}"
-    puts "Telegram: #{@telegram || 'не указан'}"
-    puts "Email: #{@email || 'не указан'}"
-    puts "GitHub: #{@git || 'не указан'}"
-    puts "-" * 30
-  end
-
-  # Метод to_s для строкового представления объекта
-  def to_s
-    "ID: #{@id || 'не указан'}, ФИО: #{@last_name} #{@first_name[0]}. #{@middle_name[0]}.," \
-    " Телефон: #{@phone || 'не указан'}, Telegram: #{@telegram || 'не указан'}," \
-    " Email: #{@email || 'не указан'}, GitHub: #{@git || 'не указан'}"
-  end
-  # Метод getInfo для краткой информации
+  # Метод для получения краткой информации о студенте
   def get_info
-    "#{last_name} #{first_name[0]}.#{middle_name[0]}.; Git: #{git || 'не указан'}; Контакт: #{contact_info || 'не указан'}"
+    "#{short_name}; Git: #{git || 'не указан'}; Контакт: #{contact_info || 'не указан'}"
   end
 
-  # Метод для получения любого доступного контакта
+  # Метод для получения доступного контакта
   def contact_info
     phone || telegram || email
-  end  
+  end
 end
--- students/student_short.rb
+++ students/student_short.rb
@@ -1,37 +1,41 @@
-class StudentShort
-    attr_reader :id, :last_name_initials, :git, :contact
-  
-    # Конструктор, принимающий объект Student
-    def self.new_from_student(student)
-      new(
-        id: student.id,
-        last_name_initials: "#{student.last_name} #{student.first_name[0]}.#{student.middle_name[0]}.",
-        git: student.git,
-        contact: student.contact_info
-      )
-    end
-  
-    # Конструктор, принимающий ID и строку с информацией
-    def self.new_from_string(id, str)
-      data = str.split('; ')
-      last_name_initials = data[0]
-      git = data[1]
-      contact = data[2]
-  
-      new(id: id, last_name_initials: last_name_initials, git: git, contact: contact)
-    end
-  
-    # Инициализация значений
-    def initialize(id:, last_name_initials:, git:, contact:)
-      @id = id
-      @last_name_initials = last_name_initials
-      @git = git
-      @contact = contact
-    end
-  
-    # Метод для строкового представления
-    def to_s
-      "ID: #{@id}, Фамилия и инициалы: #{@last_name_initials}, Git: #{@git}, Контакт: #{@contact}"
-    end
+require_relative 'person'
+
+class StudentShort < Person
+  attr_reader :git, :contact
+
+  # Конструктор, принимающий объект Student
+  def self.new_from_student(student)
+    new(
+      id: student.id,
+      last_name: student.last_name,
+      first_name: student.first_name,
+      middle_name: student.middle_name,
+      git: student.git,
+      contact: student.contact_info
+    )
   end
-  
+
+  # Конструктор, принимающий ID и строку с информацией
+  def self.new_from_string(id, str)
+    data = str.split('; ')
+    last_name_initials = data[0]
+    git = data[1]
+    contact = data[2]
+
+    last_name, initials = last_name_initials.split
+    first_name, middle_name = initials.split('.').map { |i| i + '.' }
+
+    new(id: id, last_name: last_name, first_name: first_name[0], middle_name: middle_name[0], git: git, contact: contact)
+  end
+
+  def initialize(id:, last_name:, first_name:, middle_name:, git:, contact:)
+    super(id: id, last_name: last_name, first_name: first_name, middle_name: middle_name)
+    @git = git
+    @contact = contact
+  end
+
+  # Метод для строкового представления
+  def to_s
+    "ID: #{@id}, Фамилия и инициалы: #{short_name}, Git: #{@git}, Контакт: #{@contact}"
+  end
+end
