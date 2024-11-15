61c67a0131be54b9a8cace25a031c0de155052bf
Vinyack <VinyackOfficial@gmail.com>
2024-11-16 00:47:15 +0300

    2 - testing

--- students/main.rb
+++ students/main.rb
@@ -1,11 +1,51 @@
-require_relative 'students2'
-student1 = student.new(id:1, last_name: "Дерябин1", name: "Андрей1", number: "+79628535981", tg: "@vinyack1", mail: "vinyackofficial1@gmail.com", github: "https://github.com/vinyack1")
-student2 = student.new(id:1, last_name: "Дерябин2", name: "Андрей2", number: "+79628535982", tg: "@vinyack2", mail: "vinyackofficial2@gmail.com", github: "https://github.com/vinyack2")
-student3 = student.new(id:1, last_name: "Дерябин3", name: "Андрей3", number: "+79628535983", tg: "@vinyack3", mail: "vinyackofficial3@gmail.com", github: "https://github.com/vinyack3")
+# Подключаем классы
+require_relative 'person'
+require_relative 'student'
+require_relative 'student_short'
 
-puts "Info"
-puts student1
-puts "-"
-puts student2
-puts "-"
-puts student3
+# Основная функция
+def main
+  begin
+    # Создание объекта Student
+    student = Student.new(
+      id: 1,
+      last_name: "Иванов",
+      first_name: "Иван",
+      middle_name: "Иванович",
+      phone: "+79123456789",
+      telegram: "@ivanov",
+      email: "ivanov@example.com",
+      github: "https://github.com/ivanov"
+    )
+
+    # Вывод информации о студенте
+    puts "Информация о студенте:"
+    puts student.getInfo
+    puts
+
+    # Изменение контактов
+    student.set_contacts(phone: "+79211234567", email: "new_email@example.com")
+    puts "Информация о студенте после изменения контактов:"
+    puts student.getInfo
+    puts
+
+    # Создание объекта StudentShort из Student
+    student_short = StudentShort.from_student(student)
+    puts "Информация о кратком объекте StudentShort:"
+    puts student_short
+    puts
+
+    # Создание объекта StudentShort из строки
+    info_string = "Иванов И.И.; https://github.com/ivanov; Телефон: +79211234567"
+    student_short_from_string = StudentShort.from_string(2, info_string)
+    puts "Информация о кратком объекте StudentShort из строки:"
+    puts student_short_from_string
+    puts
+
+  rescue StandardError => e
+    puts "Ошибка: #{e.message}"
+  end
+end
+
+# Вызов метода main
+main
