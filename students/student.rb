b7e2173e4c0426ab050f16c5704071c5515c6b7c
Vinyack <VinyackOfficial@gmail.com>
2024-11-16 00:12:43 +0300

    2.5 - making method to_s for info

--- students/student.rb
+++ students/student.rb
@@ -1,7 +1,7 @@
 class student
     attr_accessor :id, :last_name, :name, :number, :tg, :mail, :git
     # Конструктор, вызывается автоматически при создании объекта с помощью ClassName.new, его задача инициализировать объект и задать начальные значения для его переменных экземпляра.
-    def initialize(id: 1, last_name:, name:,number:nil,tg:,mail:,git:)
+    def initialize(id: 1, last_name:, name:,number:nil,tg:nil,mail:nil,git:nil)
         @id = id
         @last_name=last_name
         @name=name
@@ -10,4 +10,14 @@ class student
         @mail=mail
         @git=git
     end
+    def to_s
+        info = "ID: #{@id}\n"
+        info += "Фамилия #{@last_name}\n"
+        info += "Имя #{@name}\n"
+        info += "Отчество #{@otchestvo}\n"
+        info += "Номер #{@number}\n"
+        info += "Телеграм #{@tg}\n"
+        info += "Почта #{@mail}\n"
+        info += "Гит #{@git}\n"
+        info
 end
