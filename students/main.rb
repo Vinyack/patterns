5b1af896874332db773d61aeda845267ebb2c014
Vinyack <VinyackOfficial@gmail.com>
2024-11-16 00:20:11 +0300

    2 - testing puts info in main

---
+++ students/main.rb
@@ -0,0 +1,11 @@
+require_relative 'students2'
+student1 = student.new(id:1, last_name: "Дерябин1", name: "Андрей1", number: "+79628535981", tg: "@vinyack1", mail: "vinyackofficial1@gmail.com", github: "https://github.com/vinyack1")
+student2 = student.new(id:1, last_name: "Дерябин2", name: "Андрей2", number: "+79628535982", tg: "@vinyack2", mail: "vinyackofficial2@gmail.com", github: "https://github.com/vinyack2")
+student3 = student.new(id:1, last_name: "Дерябин3", name: "Андрей3", number: "+79628535983", tg: "@vinyack3", mail: "vinyackofficial3@gmail.com", github: "https://github.com/vinyack3")
+
+puts "Info"
+puts student1
+puts "-"
+puts student2
+puts "-"
+puts student3
