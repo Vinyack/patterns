700ca0db8ce967d2bd179cf412d54a0d0f99e166
Vinyack <VinyackOfficial@gmail.com>
2024-11-16 00:11:23 +0300

    2.4 - removed code duplication using attr_accessor

--- students/student.rb
+++ students/student.rb
@@ -1,4 +1,5 @@
 class student
+    attr_accessor :id, :last_name, :name, :number, :tg, :mail, :git
     # Конструктор, вызывается автоматически при создании объекта с помощью ClassName.new, его задача инициализировать объект и задать начальные значения для его переменных экземпляра.
     def initialize(id: 1, last_name:, name:,number:nil,tg:,mail:,git:)
         @id = id
@@ -8,63 +9,5 @@ class student
         @tg=tg
         @mail=mail
         @git=git
-    end
-
-    # Геттеры созданные для получения значения полей объекта Student после его создания.
-    def id()
-        @id
-    end
-
-    def last_name()
-        @last_name
-    end
-
-    def name()
-        @name
-    end
-
-    def number()
-        @number
-    end
-
-    def tg()
-        @tg
-    end
-
-    def mail()
-        @mail
-    end
-
-    def git()
-        @git
-    end
-
-    # Сеттеры созданные для изменения значения полез объекта после его создания.
-    def id=(value)
-        @id=value
-    end
-
-    def last_name=(value)
-        @last_name=value
-    end
-
-    def name=(value)
-        @name=value
-    end
-
-    def number=(value)
-        @number=value
-    end
-
-    def tg=(value)
-        @tg=value
-    end
-
-    def mail=(value)
-        @mail=value
-    end
-
-    def git=(value)
-        @git=value
     end
 end
