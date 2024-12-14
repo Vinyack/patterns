57544966ed855c22d4468d237c3a9d481ccf4049
Андрей Дерябин <VinyackOfficial@gmail.com>
2024-12-14 13:49:13 +0300

    2 - fix bugs

--- students/student.rb
+++ students/student.rb
@@ -6,77 +6,7 @@ class Student < Person
     set_contacts(phone: phone, telegram: telegram, email: email)
     super(first_name: first_name, last_name: last_name, middle_name: middle_name, git: git, id: id, contact: contact_info)
   end
-<<<<<<< HEAD
-=======
-
-  # Сеттер с валидацией для Telegram
-  private def telegram=(telegram)
-    raise "Неверный формат Telegram" unless Person.valid_telegram?(telegram)
-    @telegram = telegram
-  end
-
-  # Сеттер с валидацией для Email
-  private def email=(email)
-    raise "Неверный формат Email" unless Person.valid_email?(email)
-    @email = email
-  end
-
-  # Сеттер с валидацией для GitHub
-  def github=(github)
-    raise "Неверный формат GitHub" unless Person.valid_github?(github)
-    @github = github
-  end
-
-  # Метод для получения фамилии и инициалов
-  def short_name
-    initials = "#{@first_name[0].upcase}."
-    initials += "#{@middle_name[0].upcase}." if @middle_name
-    "#{@last_name} #{initials}"
-  end
-
-  # Метод для получения основного контакта
-  def primary_contact
-    if @phone
-      "Телефон: #{@phone}"
-    elsif @telegram
-      "Telegram: #{@telegram}"
-    elsif @email
-      "Email: #{@email}"
-    else
-      nil
-    end
-  end
-
-  # Метод для получения GitHub
-  def git_info
-    @github
-  end
-  
-    def to_s
-      str = []
-      str << "ID: #{@id}" if @id
-      str << "Firstname: #{@first_name}"
-      str << "Surname: #{@last_name}"
-      str << "Lastname: #{@middle_name}"
-      str << "Birthdate: #{@birthdate}"
-      str << "Phone: #{@phone}" if phone
-      str << "Telegram: #{@telegram}" if telegram
-      str << "Email: #{@email}" if email
-      str << "GitHub: #{@github}" if github
-      str.join('; ')
-    end
-  # Метод getInfo: возвращает краткую информацию
-  def getInfo
-    "#{short_name}; #{git_info || 'Нет GitHub'}; #{primary_contact || 'Нет контакта'}"
-  end
-
-  # Метод для установки контактов
-  def set_contacts(phone: nil, telegram: nil, email: nil)
-    raise "Неверный формат номера телефона" unless phone.nil? || Person.valid_phone?(phone)
-    raise "Неверный формат Telegram" unless telegram.nil? || Person.valid_telegram?(telegram)
-    raise "Неверный формат Email" unless email.nil? || Person.valid_email?(email)
->>>>>>> 9db67e35fb4949dc4da1f200fe6308297f4a42dd
-  
+	
   # Метод для установки контактов через сеттеры
   def set_contacts(phone: nil, telegram: nil, email: nil)
     self.phone = phone if phone
