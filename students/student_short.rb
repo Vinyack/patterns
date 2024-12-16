ccd5672cc371f802bce9fd1b0924c3a470c302aa
Андрей Дерябин <VinyackOfficial@gmail.com>
2024-12-16 05:28:40 +0300

    2 - refactoring and fix bugs with code

--- students/student_short.rb
+++ students/student_short.rb
@@ -1,40 +1,44 @@
+require_relative 'person'
 class StudentShort < Person 
-  
-    def initialize(id:, last_name_initials:, github:, contact:)
-        super(id: id)
-        @last_name_initials = last_name_initials
-        @github = github
+    attr_reader :id, :last_name_initials, :github, :contact        
+    def initialize(id:, git:, contact:, last_name_initials:)
+        @id = id
+        @git = git
         @contact = contact
+        @last_name_initials = last_name_initials
     end
 
     # Конструктор из объекта класса Student
-    def self.from_student(student)
-        raise "Передан не объект класса Student" unless student.is_a?(Student)
-  
-        new(
-            id: student.id,
-            last_name_initials: student.short_name,
-            github: student.git_info,
-            contact: student.primary_contact
+    def self.new_from_student(student)
+        self.new(
+        id: student.id,
+        git: student.git,
+        contact: student.contact,
+        last_name_initials: student.short_name
         )
     end
-  
-    # Конструктор из строки
-    def self.from_string(id, info_string)
-        data = info_string.split(";").map(&:strip)
-        raise "Некорректная строка: должно быть 3 части" unless data.size == 3
-  
-        new(
-            id: id,
-            last_name_initials: data[0],
-            github: data[1],
-            contact: data[2]
-        )
+  # Конструктор из String
+    def self.new_from_string(id:, str:)
+        student_short_init =   {}
+        params = split_str_params(str)
+        student_short_init[:id] = id
+        student_short_init[:last_name_initials] = params[0]
+        student_short_init[:git] = params[1]
+        student_short_init[:contact] = params[2..].join(' ')  # Остальные контакты
+        self.new(**student_short_init)
+    end
+    def self.split_str_params(str)
+        str.split('; ')  # Разделяем строку по символу ";" c пробелом
     end
     # Закрываем метод new
     private_class_method  :new
-    # Вывод информации
+    # Переопределение метода и вывод информации
     def to_s
-        "ID: #{@id}, ФИО: #{@last_name_initials}, GitHub: #{@github}, Контакт: #{@contact}"
+        str = []
+        str << "ID: #{@id}" if @id
+        str << "Фамилия И.О.: #{@last_name_initials}"
+        str << "GitHub: #{@git}" if @git
+        str << "#{@contact}" if @contact
+        str.join("; ")
     end
 end
