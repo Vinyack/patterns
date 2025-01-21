0514c1d6d6f1277e890ace8c49af5ef1defc0d72
Vinyack <VinyackOfficial@gmail.com>
2025-01-22 02:04:42 +0300

    3.2 - custom method sort

--- collections/array_processing.rb
+++ collections/array_processing.rb
@@ -26,11 +26,30 @@ class ArrayProcessing
 		result
 	end
 	
-	def custom_select(array)
+	def custom_select
 		return [] unless block_given?
 		result = []
-		array.each do |element|
+		@array.each do |element|
 		result << element if yield(element) # добавляем элемент в результат, если блок true
 		end
 		result
+	end
+	
+	def custom_sort
+		return @array unless block_given?
+
+		sorted = @array.dup # создание копии массива 
+
+		loop do
+			swapped = false # флаг, который указывает, были ли перестановки
+			(0...sorted.size - 1).each do |i|
+			if yield(sorted[i], sorted[i + 1]) # вызываем блок для сравнения пары
+				sorted[i], sorted[i + 1] = sorted[i + 1], sorted[i]
+				swapped = true # устанавливаем флаг, если была перестановка
+			end
+			end
+			break unless swapped # если не было перестановок, сортировка завершена
+		end
+
+		sorted # возвращаем отсортированный массив
 	end
